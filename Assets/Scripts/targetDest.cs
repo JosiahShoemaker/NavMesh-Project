36280864436292e49ec9b1b15b5b12d043526548
Josiah <jbshoes722@gmail.com>
2021-03-08 17:54:14 -0500

    Initial commit

---
+++ Assets/Scripts/targetDest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class targetDest : MonoBehaviour
+{
+     //agent
+    public NavMeshAgent agent;
+    //position of target
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetDestination();
+    }
+
+    void SetDestination()
+    {
+        // position to vector3
+        Vector3 targetVector = target.transform.position;
+        //agent.SetDestination(Vector3)
+        agent.SetDestination(targetVector);
+    }
+}
+
+
